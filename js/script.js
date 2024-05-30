e41a6ac01a9ceb38be0134923deaf1b8f33860fe
gabimezze <gabrielagomescezar@gmail.com>
2024-05-30 01:23:33 -0300

    update all

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menu = document.getElementById('menu');
+    var overlay = document.getElementById('overlay');
+    menu.classList.toggle('open');
+    overlay.classList.toggle('show');
+}
