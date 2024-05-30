c111d1dcfa1509b35380a1fbf294e153255cb9c2
gabimezze <gabrielagomescezar@gmail.com>
2024-05-30 19:42:53 -0300

    update css, html e js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
+// MENU DE LINKS
 function toggleMenu() {
     var menu = document.getElementById('menu');
     var overlay = document.getElementById('overlay');
     menu.classList.toggle('open');
     overlay.classList.toggle('show');
+}
+
+// COMO AJUDAR
+function toggleParagraph(paraId) {
+    var para = document.getElementById(paraId);
+    var arrow = para.previousElementSibling.querySelector('.arrow');
+
+    if (para.style.display === "none" || para.style.display === "") {
+        para.style.display = "block";
+        arrow.innerHTML = "▲";
+    } else {
+        para.style.display = "none";
+        arrow.innerHTML = "▼";
+    }
 }
