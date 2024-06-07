e8c2656b3908c10c1d4c37b8949f7878414198eb
gabimezze <gabrielagomescezar@gmail.com>
2024-06-07 15:27:20 -0300

    update css, html e js

--- js/script.js
+++ js/script.js
@@ -19,3 +19,17 @@ function toggleParagraph(paraId) {
         arrow.innerHTML = "▼";
     }
 }
+
+// CHATBOT
+
+window.watsonAssistantChatOptions = {
+    integrationID: "5d63d7c9-8ca8-4087-8c59-0a0f181605aa", // The ID of this integration.
+    region: "au-syd", // The region your integration is hosted in.
+    serviceInstanceID: "18d70c0a-e82c-4461-af29-50e5ba73a926", // The ID of your service instance.
+    onLoad: async (instance) => { await instance.render(); }
+};
+setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+    document.head.appendChild(t);
+});
